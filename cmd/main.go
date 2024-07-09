7d529a8de6554e12ddb423fc75a413b2c455fd37
ennaragon <enno.aragon@gmail.com>
2024-07-09 07:09:34 -0700

    added new files

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	"fmt"
-    "html/template"
+//	"fmt"
+//    "html/template"
 	"log"
 	"net/http"
+    "ennoaragon/internal/handler"
 )
 type Film struct {
     Title string
@@ -12,21 +13,21 @@ type Film struct {
 }
 
 func main() {
-    fmt.Println("hello world")
-
-    h1 := func(w http.ResponseWriter, r *http.Request) {
-        tmpl := template.Must(template.ParseFiles("./public/index.html"))
-        films := map[string][]Film{
-            "Films": {
-                {Title: "Casablanca", Director: "Michael Curtiz"},
-                {Title: "Cool Hand Luke", Director: "Stuart Rosenberg"},
-                {Title: "Bullitt", Director: "Peter Yates"},
-            },
-        }
-        tmpl.Execute(w, films)
-    }
-    http.HandleFunc("/", h1)
-    log.Fatal(http.ListenAndServe(":8080", nil))
+//    fmt.Println("hello world")
+//
+//    h1 := func(w http.ResponseWriter, r *http.Request) {
+//        tmpl := template.Must(template.ParseFiles("./public/index.html"))
+//        films := map[string][]Film{
+//            "Films": {
+//                {Title: "Casablanca", Director: "Michael Curtiz"},
+//                {Title: "Cool Hand Luke", Director: "Stuart Rosenberg"},
+//                {Title: "Bullitt", Director: "Peter Yates"},
+//            },
+//        }
+//        tmpl.Execute(w, films)
+//    }
+//    http.HandleFunc("/", h1)
+//    log.Fatal(http.ListenAndServe(":8080", nil))
 
 
     mux := http.NewServeMux()
---
+++ go.mod
@@ -0,0 +1,3 @@
+module ennoaragon
+
+go 1.22.2
