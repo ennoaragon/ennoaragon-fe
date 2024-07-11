c9f5203f6bb9ce139a16309c5809083113ccc472
ennaragon <enno.aragon@gmail.com>
2024-07-11 03:03:34 -0700

    adding files to the web

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,8 +11,14 @@ module.exports = {
      colors: {
        primary: '#3E64FF',
        secondary: '#FF7F50',
-       'custom-background': '#F0F0F0',
+       'custom-background': '#181818',
+       'custom-gradiant': '#1c282e',
      },
+    backgroundImage: {
+        'gradient-45': 'linear-gradient(60deg, var(--tw-gradient-stops))',
+        'gradient-double45': 'linear-gradient(150deg, var(--tw-gradient-stops)), linear-gradient(240deg, var(--tw-gradient-stops))',
+        'gradient-fancy': 'linear-gradient(217deg, #1c282e,#181818 30.71%), linear-gradient(127deg, rgba(0,255,0,.8), rgba(0,255,0,0) 70.71%), linear-gradient(336deg, rgba(0,0,255,.8), rgba(0,0,255,0) 70.71%)',
+      },
      fontFamily: {
         serif: ['serif'],
         Lato: ['Lato', ...defaultTheme.fontFamily.sans]
